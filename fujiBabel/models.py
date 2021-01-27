b4c8552fef1f83c3d6375c48f27ddf8d617c677b
Omar <o.ouhbad@gmail.com>
2021-01-27 18:54:28 +0100

    bdd + bug 2j dans latest

--- fujiBabel/models.py
+++ fujiBabel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Bibliotheque(models.Model):
+    mangas_id = models.IntegerField()
+    notified = models.BooleanField(default=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+
+class ChapitreLu(models.Model):
+    mangas_id = models.ForeignKey(Bibliotheque, on_delete=models.CASCADE)
+    chapter_id = models.IntegerField()
+    date_read = models.DateTimeField(auto_now_add=True)
